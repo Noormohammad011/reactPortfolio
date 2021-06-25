4bd1568ef704151cea7110483031e16654d68f22
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-06-26 01:49:27 +0600

    'testing'

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Portfolio App with netlify
         </a>
       </header>
     </div>
