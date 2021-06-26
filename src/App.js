f4e258977a0d0b356058c97a8dd62c23f903a125
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-06-27 01:52:45 +0600

    4. Global Style with styled section and work on AboutSection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import AboutUs from './pages/AboutUs'
-
+import GlobalStyles from './components/GlobalStyles'
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
+      <GlobalStyles/>
      <AboutUs/>
     </div>
   )
