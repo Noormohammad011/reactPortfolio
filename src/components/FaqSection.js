06d5c7b0fee8e587fd0e39cb1e2d8bcd3dad8805
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-06-27 01:15:57 +0600

    3.Faq Section Html

---
+++ src/components/FaqSection.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const FaqSection = () => {
+    return (
+      <div className='faq'>
+        <h2>
+          Any question<span>FAQ</span>
+        </h2>
+        <div className='Question'>
+          <h4>How Do I start</h4>
+          <div className='answer'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              iusto?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              velit!
+            </p>
+          </div>
+        </div>
+        <div className='Question'>
+          <h4>How Do I start</h4>
+          <div className='answer'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              iusto?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              velit!
+            </p>
+          </div>
+        </div>
+        <div className='Question'>
+          <h4>How Do I start</h4>
+          <div className='answer'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              iusto?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              velit!
+            </p>
+          </div>
+        </div>
+        <div className='Question'>
+          <h4>How Do I start</h4>
+          <div className='answer'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              iusto?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              velit!
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default FaqSection
