5170c15073e842137ea54195a1c89a5fa430b7fd
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-06-28 02:02:41 +0600

    7. Navbar is created and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AboutUs from './pages/AboutUs'
 import GlobalStyles from './components/GlobalStyles'
+import Nav from './components/Nav'
 const App = () => {
   return (
-    <div className="App">
-      <GlobalStyles/>
-     <AboutUs/>
+    <div className='App'>
+      <GlobalStyles />
+      <Nav />
+      <AboutUs />
     </div>
   )
 }
